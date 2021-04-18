56e23b8024a5b48ee1c4f4fd9d6f6b022028bd3f
hetzlefetz <distorted.peace@gmail.com>
2021-04-18 20:37:10 +0200

    Fix Encoding issues

--- DUD-E2 - Umlaute suchen und ersetzen/DUD-E2/HtmlConv.java
+++ DUD-E2 - Umlaute suchen und ersetzen/DUD-E2/HtmlConv.java
@@ -5,29 +5,35 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class HtmlConv {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
 		var fileNameIn = "./datei1702_vor.html";
 		var fileNameOut = "./datei1702_nach.html";
 
-		try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(new File(fileNameIn)));
-				BufferedWriter bufferedFileWriter = new BufferedWriter(new FileWriter(fileNameOut ));) {
-			
+		var inFile = new File(fileNameIn);
+		var outFile = new File(fileNameOut);
+		var charset = Charset.forName("ISO8859_1");
+
+		if (!inFile.exists()) {
+			throw new FileNotFoundException("Missing in file. Expected to find: " + inFile.getAbsolutePath());
+		}
+		if (outFile.exists()) {
+			outFile.delete();
+		}
+
+		try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(inFile, charset));
+				BufferedWriter bufferedFileWriter = new BufferedWriter(new FileWriter(fileNameOut, charset));) {
+
 			String currentLine = null;
 			while ((currentLine = bufferedFileReader.readLine()) != null) {
-				bufferedFileWriter.write(
-						currentLine
-						.replace("ä","&auml;")
-						.replace("Ä", "&Auml;")
-						.replace("ö", "&ouml;")
-						.replace("Ö", "&Ouml;")
-						.replace("ü", "&uuml;")
-						.replace("Ü", "&Uuml;")
-						.replace("ß", "&szlig;")
-						.replace("ẞ", "&#7838;"));
+				System.out.println(currentLine);
+				bufferedFileWriter.write(currentLine.replace("ä", "&auml;").replace("Ä", "&Auml;")
+						.replace("ö", "&ouml;").replace("Ö", "&Ouml;").replace("ü", "&uuml;").replace("Ü", "&Uuml;")
+						.replace("ß", "&szlig;").replace("ẞ", "&#7838;"));
 				bufferedFileWriter.newLine();
 			}
 

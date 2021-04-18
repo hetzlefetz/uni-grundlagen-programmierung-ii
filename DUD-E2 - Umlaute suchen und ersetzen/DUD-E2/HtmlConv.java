8b41c830682a11b7d00b7b7e93206d7c49980ca1
hetzlefetz <distorted.peace@gmail.com>
2021-04-18 21:58:07 +0200

    Added Comments, Exception handling and exit codes

--- DUD-E2 - Umlaute suchen und ersetzen/DUD-E2/HtmlConv.java
+++ DUD-E2 - Umlaute suchen und ersetzen/DUD-E2/HtmlConv.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,7 +8,13 @@ import java.nio.charset.Charset;
 
 public class HtmlConv {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	/**
+	 * 
+	 * @param args - Not used
+	 * @return exitCodes -1: input file not found -2: I/O Excpetion during
+	 *         conversation 0 : All good
+	 */
+	public static void main(String[] args) {
 
 		var fileNameIn = "./datei1702_vor.html";
 		var fileNameOut = "./datei1702_nach.html";
@@ -19,8 +24,10 @@ public class HtmlConv {
 		var charset = Charset.forName("ISO8859_1");
 
 		if (!inFile.exists()) {
-			throw new FileNotFoundException("Missing in file. Expected to find: " + inFile.getAbsolutePath());
+			System.out.println("Missing in file. Expected to find: " + inFile.getAbsolutePath());
+			System.exit(-1);
 		}
+
 		if (outFile.exists()) {
 			outFile.delete();
 		}
@@ -30,21 +37,15 @@ public class HtmlConv {
 
 			String currentLine = null;
 			while ((currentLine = bufferedFileReader.readLine()) != null) {
-				System.out.println(currentLine);
 				bufferedFileWriter.write(currentLine.replace("ä", "&auml;").replace("Ä", "&Auml;")
 						.replace("ö", "&ouml;").replace("Ö", "&Ouml;").replace("ü", "&uuml;").replace("Ü", "&Uuml;")
 						.replace("ß", "&szlig;").replace("ẞ", "&#7838;"));
 				bufferedFileWriter.newLine();
 			}
-
-		} catch (FileNotFoundException e) {
-			System.out.println("File Not found!");
-			System.out.println(e.getLocalizedMessage());
 		} catch (IOException e) {
-			System.out.println("IO Exception occured!");
+			System.out.println("I/O Exception occured!");
 			System.out.println(e.getLocalizedMessage());
+			System.exit(-2);
 		}
-
 	}
-
 }
